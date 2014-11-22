cf13cc19148cbe5a3b1e01708c778fc0e266e37a
wolske <st4rbux@gmail.com>
2014-11-22 00:13:51 -0500

    change smallchange/SMALLCHANGE/SMC to barcoin/etc

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("BAR");
+    case mBTC: return QString("mBAR");
+    case uBTC: return QString::fromUtf8("μBAR");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("BarCoin");
+    case mBTC: return QString("Milli-BarCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-BarCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
